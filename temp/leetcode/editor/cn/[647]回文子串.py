c92835600b9fc17c2649556214e899fb34abd2e2
wuzhijian <wuch1k1n@qq.com>
2023-07-28 11:17:08 +0800

    commit 657

--- temp/leetcode/editor/cn/[647]回文子串.py
+++ temp/leetcode/editor/cn/[647]回文子串.py
@@ -30,10 +30,14 @@
 
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution(object):
-    def countSubstrings(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        
+    def countSubstrings(self, s: str) -> int:
+        res = 0
+        n = len(s)
+        for i in range(0, 2 * n - 1):
+            l, r = i // 2, i // 2 + i % 2
+            while l >= 0 and r < n and s[l] == s[r]:
+                res += 1
+                l -= 1
+                r += 1
+        return res
 # leetcode submit region end(Prohibit modification and deletion)
