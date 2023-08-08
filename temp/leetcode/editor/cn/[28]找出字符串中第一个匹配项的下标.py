9163d869f4b563a80711184f48e6000c49577fd7
wuzhijian <wuch1k1n@qq.com>
2023-08-08 10:13:25 +0800

    commit 28

--- temp/leetcode/editor/cn/[28]找出字符串中第一个匹配项的下标.py
+++ temp/leetcode/editor/cn/[28]找出字符串中第一个匹配项的下标.py
@@ -38,39 +38,48 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        def KMP(s: str, p: str):
-            nex = getNext(p)
-            i = 0
-            j = 0  # 分别是s和p的指针
-            while i < len(s) and j < len(p):
-                if j == -1 or s[i] == p[j]:  # j==-1是由于j=next[j]产生
-                    i += 1
-                    j += 1
-                else:
-                    j = nex[j]
-            if j == len(p):  # j走到了末尾，说明匹配到了
-                return i - j
+        # def KMP(s: str, p: str):
+        #     nex = getNext(p)
+        #     i = 0
+        #     j = 0  # 分别是s和p的指针
+        #     while i < len(s) and j < len(p):
+        #         if j == -1 or s[i] == p[j]:  # j==-1是由于j=next[j]产生
+        #             i += 1
+        #             j += 1
+        #         else:
+        #             j = nex[j]
+        #     if j == len(p):  # j走到了末尾，说明匹配到了
+        #         return i - j
+        #     else:
+        #         return -1
+        #
+        # def getNext(p: str):
+        #     nex = [0] * (len(p) + 1)
+        #     nex[0] = -1
+        #     i = 0
+        #     j = -1
+        #     while i < len(p):
+        #         if j == -1 or p[i] == p[j]:
+        #             i += 1
+        #             j += 1
+        #             nex[i] = j  # 这是最大的不同：记录next[i]
+        #         else:
+        #             j = nex[j]
+        #
+        #     return nex
+        #
+        # return KMP(haystack, needle)
+
+        m, n = len(haystack), len(needle)
+        for i in range(m):
+            for j in range(0, n):
+                if i + j >= m or haystack[i + j] != needle[j]:
+                    break
             else:
-                return -1
-
-        def getNext(p: str):
-            nex = [0] * (len(p) + 1)
-            nex[0] = -1
-            i = 0
-            j = -1
-            while i < len(p):
-                if j == -1 or p[i] == p[j]:
-                    i += 1
-                    j += 1
-                    nex[i] = j  # 这是最大的不同：记录next[i]
-                else:
-                    j = nex[j]
-
-            return nex
-
-        return KMP(haystack, needle)
+                return i
+        return -1
 # leetcode submit region end(Prohibit modification and deletion)
 
 
 if __name__ == '__main__':
-    Solution().strStr("leetcode", "leeto")
+    print(Solution().strStr("leetcode", "leeto"))
