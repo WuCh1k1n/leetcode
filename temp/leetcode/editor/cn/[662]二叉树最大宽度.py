2f27d7ca5aae52136b60aeab22b6e1eaef4e4c24
wuzhijian <wuch1k1n@qq.com>
2023-08-15 15:55:10 +0800

    commit 662

--- temp/leetcode/editor/cn/[662]二叉树最大宽度.py
+++ temp/leetcode/editor/cn/[662]二叉树最大宽度.py
@@ -77,15 +77,16 @@
 #         self.right = right
 class Solution:
     def widthOfBinaryTree(self, root: TreeNode) -> int:
+        res = 0
+        cur_depth = left = 0
         queue = [(root, 0, 0)]
-        cur_depth = left = ans = 0
         for node, depth, pos in queue:
             if node:
-                queue.append((node.left, depth + 1, pos * 2))
-                queue.append((node.right, depth + 1, pos * 2 + 1))
-                if cur_depth != depth:
+                queue.append((node.left, depth + 1, pos * 2 + 1))
+                queue.append((node.right, depth + 1, pos * 2 + 2))
+                if depth != cur_depth:
                     cur_depth = depth
                     left = pos
-                ans = max(pos - left + 1, ans)
-        return ans
+                res = max(res, pos - left + 1)
+        return res
 # leetcode submit region end(Prohibit modification and deletion)
