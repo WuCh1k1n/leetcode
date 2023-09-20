e5694a2ad3f02564c41c6091c10d6505131ae629
wuzhijian <wuch1k1n@qq.com>
2023-09-20 10:28:25 +0800

    commit offer 03

--- temp/leetcode/editor/cn/[剑指 Offer 03]数组中重复的数字.py
+++ temp/leetcode/editor/cn/[剑指 Offer 03]数组中重复的数字.py
@@ -21,13 +21,29 @@
 
 
 # leetcode submit region begin(Prohibit modification and deletion)
+from collections import Counter
+from typing import List
+
+
 class Solution:
     def findRepeatNumber(self, nums: List[int]) -> int:
-        counter = dict()
-        for num in nums:
-            counter.setdefault(num, 0)
-            counter[num] += 1
-            if counter[num] > 1:
-                return num
+        # counter = Counter()
+        # for num in nums:
+        #     counter[num] += 1
+        #     if counter[num] > 1:
+        #         return num
+        # return -1
+
+        i = 0
+        while i < len(nums):
+            if nums[i] == i:
+                i += 1
+                continue
+            if nums[nums[i]] == nums[i]: return nums[i]
+            nums[nums[i]], nums[i] = nums[i], nums[nums[i]]
         return -1
 # leetcode submit region end(Prohibit modification and deletion)
+
+
+if __name__ == '__main__':
+    Solution().findRepeatNumber([2, 3, 1, 0, 2, 5, 3])
