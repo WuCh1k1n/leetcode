8edfe8db9773f308143fe613e412c24c523c9821
wuzhijian <wuch1k1n@qq.com>
2023-09-20 10:35:26 +0800

    commit 746

--- temp/leetcode/editor/cn/[746]使用最小花费爬楼梯.py
+++ temp/leetcode/editor/cn/[746]使用最小花费爬楼梯.py
@@ -35,15 +35,14 @@
 
 
 # leetcode submit region begin(Prohibit modification and deletion)
+from typing import List
+
+
 class Solution(object):
-    def minCostClimbingStairs(self, cost):
-        """
-        :type cost: List[int]
-        :rtype: int
-        """
-        dp = [0] * len(cost)
-        dp[0], dp[1] = cost[0], cost[1]
-        for i in range(2, len(cost)):
-            dp[i] = cost[i] + min(dp[i - 1], dp[i - 2])
-        return min(dp[-1], dp[-2])
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        n = len(cost)
+        dp = [0] * (n + 1)
+        for i in range(2, len(dp)):
+            dp[i] = min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2])
+        return dp[-1]
 # leetcode submit region end(Prohibit modification and deletion)
