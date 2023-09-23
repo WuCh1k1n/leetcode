462b799025c7acc3c163e99e1d5036ce345daac4
wuzhijian <wuch1k1n@qq.com>
2023-09-23 23:07:55 +0800

    commit 213

--- temp/leetcode/editor/cn/[213]打家劫舍 II.py
+++ temp/leetcode/editor/cn/[213]打家劫舍 II.py
@@ -46,12 +46,12 @@ from typing import List
 
 class Solution(object):
     def rob(self, nums: List[int]) -> int:
-        def my_rob(nums: List[int]) -> int:
+        def helper(nums: List[int]) -> int:
             cur = pre = 0
             for num in nums:
                 cur, pre = max(pre + num, cur), cur
             return cur
-        return max(my_rob(nums[:-1]), my_rob(nums[1:])) if len(nums) != 1 else nums[0]
+        return max(helper(nums[:-1]), helper(nums[1:])) if len(nums) != 1 else nums[0]
 # leetcode submit region end(Prohibit modification and deletion)
 
 
