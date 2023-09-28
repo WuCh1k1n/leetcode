c8790629b0aae6c2e04e165f4b30821be59aa4eb
wuzhijian <wuch1k1n@qq.com>
2023-09-28 13:56:26 +0800

    commit 84

--- temp/leetcode/editor/cn/[84]柱状图中最大的矩形.py
+++ temp/leetcode/editor/cn/[84]柱状图中最大的矩形.py
@@ -39,10 +39,11 @@ class Solution(object):
                 mono_stack.pop()
             left[i] = mono_stack[-1] if mono_stack else -1
             mono_stack.append(i)
-        ans = max((right[i] - left[i] - 1) * heights[i] for i in range(n)) if n > 0 else 0
-        return ans
+        res = max((right[i] - left[i] - 1) * heights[i] for i in range(n)) if n > 0 else 0
+        return res
 # leetcode submit region end(Prohibit modification and deletion)
 
 
 if __name__ == '__main__':
-    Solution().largestRectangleArea([2, 1, 5, 6, 2, 3])
+    print(Solution().largestRectangleArea([2, 1, 5, 6, 2, 3]))
+    print(Solution().largestRectangleArea([2, 4]))
